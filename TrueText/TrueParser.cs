72b33a37d13b153b9137725aceeaf2160f757e9f
Francis Judge <francis@fjsolutions.co.za>
2024-09-13 17:43:50 +0200

    Started implementing a TruParser static class as the default place to parse stings into values

---
+++ TrueText/TrueParser.cs
@@ -0,0 +1,254 @@
+﻿namespace TrueText;
+
+public static class TrueParser
+{
+    /// <summary>
+    /// Parses a <see cref="String"/> value as a <see cref="string"/>
+    /// </summary>
+    /// <param name="value">The value to parse as a <see cref="String"/></param>
+    /// <returns>A <see cref="Option{TValue}"/></returns>
+    public static Option<string> ParseString(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return new Some<string>(string.Empty);
+
+        return new Some<string>(value);
+    }
+
+    /// <summary>
+    /// Parses a <see cref="String"/> value as an <see cref="sbyte"/>
+    /// </summary>
+    /// <param name="value">The value to parse as an <see cref="sbyte"/></param>
+    /// <returns>A <see cref="Option{TValue}"/></returns>
+    public static Option<sbyte> ParseInt8(string value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            if (sbyte.TryParse(value, out var val))
+                return new Some<sbyte>(val);
+        }
+
+        return new None<sbyte>();
+    }
+
+    /// <summary>
+    /// Parses a <see cref="String"/> value as an <see cref="short"/>
+    /// </summary>
+    /// <param name="value">The value to parse as an <see cref="short"/></param>
+    /// <returns>A <see cref="Option{TValue}"/></returns>
+    public static Option<short> ParseInt16(string value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            if (short.TryParse(value, out var val))
+                return new Some<short>(val);
+        }
+
+        return new None<short>();
+    }
+
+    /// <summary>
+    /// Parses a <see cref="String"/> value as an <see cref="int"/>
+    /// </summary>
+    /// <param name="value">The value to parse as an <see cref="int"/></param>
+    /// <returns>A <see cref="Option{TValue}"/></returns>
+    public static Option<int> ParseInt32(string value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            if (int.TryParse(value, out var val))
+                return new Some<int>(val);
+        }
+
+        return new None<int>();
+    }
+
+    /// <summary>
+    /// Parses a <see cref="String"/> value as an <see cref="long"/>
+    /// </summary>
+    /// <param name="value">The value to parse as an <see cref="long"/></param>
+    /// <returns>A <see cref="Option{TValue}"/></returns>
+    public static Option<long> ParseInt64(string value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            if (long.TryParse(value, out var val))
+                return new Some<long>(val);
+        }
+
+        return new None<long>();
+    }
+
+    /// <summary>
+    /// Parses a <see cref="String"/> value as an <see cref="byte"/>
+    /// </summary>
+    /// <param name="value">The value to parse as an <see cref="byte"/></param>
+    /// <returns>A <see cref="Option{TValue}"/></returns>
+    public static Option<byte> ParseUInt8(string value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            if (byte.TryParse(value, out var val))
+                return new Some<byte>(val);
+        }
+
+        return new None<byte>();
+    }
+
+    /// <summary>
+    /// Parses a <see cref="String"/> value as an <see cref="ushort"/>
+    /// </summary>
+    /// <param name="value">The value to parse as an <see cref="ushort"/></param>
+    /// <returns>A <see cref="Option{TValue}"/></returns>
+    public static Option<ushort> ParseUInt16(string value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            if (ushort.TryParse(value, out var val))
+                return new Some<ushort>(val);
+        }
+
+        return new None<ushort>();
+    }
+
+    /// <summary>
+    /// Parses a <see cref="String"/> value as an <see cref="uint"/>
+    /// </summary>
+    /// <param name="value">The value to parse as an <see cref="uint"/></param>
+    /// <returns>A <see cref="Option{TValue}"/></returns>
+    public static Option<uint> ParseUInt32(string value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            if (uint.TryParse(value, out var val))
+                return new Some<uint>(val);
+        }
+
+        return new None<uint>();
+    }
+
+    /// <summary>
+    /// Parses a <see cref="String"/> value as an <see cref="ulong"/>
+    /// </summary>
+    /// <param name="value">The value to parse as an <see cref="ulong"/></param>
+    /// <returns>A <see cref="Option{TValue}"/></returns>
+    public static Option<ulong> ParseUInt64(string value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            if (ulong.TryParse(value, out var val))
+                return new Some<ulong>(val);
+        }
+
+        return new None<ulong>();
+    }
+
+    /// <summary>
+    /// Parses a <see cref="String"/> value as an <see cref="float"/>
+    /// </summary>
+    /// <param name="value">The value to parse as an <see cref="float"/></param>
+    /// <returns>A <see cref="Option{TValue}"/></returns>
+    public static Option<float> ParseSingle(string value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            if (float.TryParse(value, out var val))
+                return new Some<float>(val);
+        }
+
+        return new None<float>();
+    }
+
+    /// <summary>
+    /// Parses a <see cref="String"/> value as an <see cref="double"/>
+    /// </summary>
+    /// <param name="value">The value to parse as an <see cref="double"/></param>
+    /// <returns>A <see cref="Option{TValue}"/></returns>
+    public static Option<double> ParseDouble(string value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            if (double.TryParse(value, out var val))
+                return new Some<double>(val);
+        }
+
+        return new None<double>();
+    }
+
+    /// <summary>
+    /// Parses a <see cref="String"/> value as an <see cref="decimal"/>
+    /// </summary>
+    /// <param name="value">The value to parse as an <see cref="decimal"/></param>
+    /// <returns>A <see cref="Option{TValue}"/></returns>
+    public static Option<decimal> ParseDecimal(string value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            if (decimal.TryParse(value, out var val))
+                return new Some<decimal>(val);
+        }
+
+        return new None<decimal>();
+    }
+
+    /// <summary>
+    /// Parses a <see cref="String"/> value as an <see cref="Guid"/>
+    /// </summary>
+    /// <param name="value">The value to parse as an <see cref="Guid"/></param>
+    /// <returns>A <see cref="Option{TValue}"/></returns>
+    public static Option<Guid> ParseGuid(string value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            if (Guid.TryParse(value, out var val))
+                return new Some<Guid>(val);
+        }
+
+        return new None<Guid>();
+    }
+    
+    // DateTime
+    // DateOnly
+    // TimeOnly
+}
+
+public abstract record Option<TValue>
+{
+    private protected Option()
+    {
+    }
+
+    public TResult Match<TResult>(Func<TValue, TResult> some, Func<TResult> none)
+    {
+        return this switch
+        {
+            Some<TValue> s => some(s.Value),
+            None<TValue> _ => none(),
+            _ => throw new TrueTextException("Unknown Option type")
+        };
+    }
+
+    public Option<TResult> Map<TResult>(Func<TValue, TResult> mapper)
+    {
+        return this switch
+        {
+            Some<TValue> s => new Some<TResult>(mapper(s.Value)),
+            None<TValue> _ => new None<TResult>(),
+            _ => throw new TrueTextException("Unknown Option type")
+        };
+    }
+
+    public Option<TResult> Bind<TResult>(Func<TValue, Option<TResult>> binder)
+    {
+        return this switch
+        {
+            Some<TValue> s => binder(s.Value),
+            None<TValue> _ => new None<TResult>(),
+            _ => throw new TrueTextException("Unknown Option type")
+        };
+    }
+}
+
+internal sealed record Some<T>(T Value) : Option<T>;
+
+internal sealed record None<T> : Option<T>;
---
+++ TrueTextTests/TruerParserTests.cs
@@ -0,0 +1,427 @@
+﻿namespace TrueTextTests;
+
+using static TrueText.TrueParser;
+
+public class TruerParserTests
+{
+    [Fact]
+    public void ParseStringTest()
+    {
+        ParseString("").Match(
+            some: s => string.Empty,
+            none: () =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseSomeInt8Test()
+    {
+        ParseInt8("7").Match(
+            some: value =>
+            {
+                Assert.Equal(7, value);
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseNoneInt8Test()
+    {
+        ParseInt8("A").Match(
+            some: value =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.True(true);
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseSomeInt16Test()
+    {
+        ParseInt16("7").Match(
+            some: value =>
+            {
+                Assert.Equal(7, value);
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseNoneInt16Test()
+    {
+        ParseInt16("A").Match(
+            some: value =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.True(true);
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseSomeInt32Test()
+    {
+        ParseInt32("7").Match(
+            some: value =>
+            {
+                Assert.Equal(7, value);
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseNoneInt32Test()
+    {
+        ParseInt32("A").Match(
+            some: value =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.True(true);
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseSomeInt64Test()
+    {
+        ParseInt64("7").Match(
+            some: value =>
+            {
+                Assert.Equal(7, value);
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseNoneInt64Test()
+    {
+        ParseInt64("A").Match(
+            some: value =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.True(true);
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseSomeUInt8Test()
+    {
+        ParseUInt8("7").Match(
+            some: value =>
+            {
+                Assert.Equal(7, value);
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseNoneUInt8Test()
+    {
+        ParseUInt8("A").Match(
+            some: value =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.True(true);
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseSomeUInt16Test()
+    {
+        ParseUInt16("7").Match(
+            some: value =>
+            {
+                Assert.Equal(7, value);
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseNoneUInt16Test()
+    {
+        ParseUInt16("A").Match(
+            some: value =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.True(true);
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseSomeUInt32Test()
+    {
+        ParseUInt32("7").Match(
+            some: value =>
+            {
+                Assert.True(7 == value);
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseNoneUInt32Test()
+    {
+        ParseUInt32("A").Match(
+            some: value =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.True(true);
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseSomeUInt64Test()
+    {
+        ParseUInt64("7").Match(
+            some: value =>
+            {
+                Assert.True(7 == value);
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseNoneUInt64Test()
+    {
+        ParseUInt64("A").Match(
+            some: value =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.True(true);
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseSomeFloatTest()
+    {
+        ParseSingle("7.0").Match(
+            some: value =>
+            {
+                Assert.Equal(7.0, value, 0.0001);
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseNoneFloatTest()
+    {
+        ParseUInt64("A").Match(
+            some: value =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.True(true);
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseSomeDoubleTest()
+    {
+        ParseDouble("7.0").Match(
+            some: value =>
+            {
+                Assert.Equal(7.0, value);
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseNoneDoubleTest()
+    {
+        ParseUInt64("A").Match(
+            some: value =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.True(true);
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseSomeDecimalTest()
+    {
+        ParseDecimal("7.0").Match(
+            some: value =>
+            {
+                Assert.Equal(7.0M, value);
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseNoneDecimalTest()
+    {
+        ParseUInt64("A").Match(
+            some: value =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.True(true);
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseSomeGuidTest()
+    {
+        ParseGuid("B808889A-3545-4DD7-A4C5-5AF6FBAAD098").Match(
+            some: value =>
+            {
+                Assert.Equal(Guid.Parse("B808889A35454DD7A4C55AF6FBAAD098"), value);
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            }
+        );
+    }
+
+    [Fact]
+    public void ParseNoneGuidTest()
+    {
+        ParseUInt64("B808889A-3545-4DD7-A4C5-5AF6FBAAD09Z").Match(
+            some: value =>
+            {
+                Assert.Fail();
+                return string.Empty;
+            },
+            none: () =>
+            {
+                Assert.True(true);
+                return string.Empty;
+            }
+        );
+    }
+}
